0daed0ba6a5490484945904761fd22a2477af02a
Burak Talha Sumer <alptng72@gmail.com>
2021-09-03 14:55:51 +0300

    Bank005 | The entities layer has updated

---
+++ Bank_Application/src/entities/concretes/superClasses/Staffs.java
@@ -0,0 +1,41 @@
+package entities.concretes.superClasses;
+
+import javax.persistence.*;
+
+
+@Table(name="staffs")
+public class Staffs{
+	
+	@Id
+	@Column(name="staffs_id")
+    private int staffs_id;
+	
+	@Column(name="authority")
+    private String authority;
+	
+	@Column(name="staffs_name")
+    private String staffs_name;
+
+
+
+    public int getStaffs_id() {
+        return staffs_id;
+    }
+    public void setStaffs_id(int staffs_id) {
+        this.staffs_id = staffs_id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public String getStaffs_name() {
+        return staffs_name;
+    }
+    public void setStaffs_name(String staffs_name) {
+        this.staffs_name = staffs_name;
+    }
+}
